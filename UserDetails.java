6cd23a489c873207bac06b5e8e0523abcd51590b
Prasad <prasadj007official@gmail.com>
2022-03-03 08:49:18 +0530

    ADDED:UserDetails.java to UC1

---
+++ UserDetails.java
@@ -0,0 +1,45 @@
+package com.bridgelabz.regex;
+
+public class UserDetails {
+	public String firstName;
+    public String lastName;
+	public String email;
+	public String mobile;
+	public String password;
+	
+	
+	public String toString() {
+		return "User [firstName="+firstName+",lastName="+lastName+",email="+email+",mobile="
+						+mobile+",password"+password+"]";
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName=firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName=lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile=mobile;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+}
